94ee5a0b6965a95eafd7a1891768df0de6b89ed6
iromashko <iromashko@me.com>
2020-06-09 02:04:00 +0300

    add auth service

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { RegisterRequestInterface } from '../types/registerRequest.interface';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { AuthResponseInterface } from '../types/auth-response.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(private http: HttpClient) {}
+  register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/users';
+    return this.http
+      .post<AuthResponseInterface>(url, data)
+      .pipe(map((response: AuthResponseInterface) => response.user));
+  }
+}
